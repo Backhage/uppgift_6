ccef5526d26b937b78e5259f093faf72f7d7c15e
Eric Backhage <eric.backhage@gmail.com>
2014-09-18 23:08:13 +0200

    Added functionality for calculating parallel coupled resistors correcly.

--- resistance.c
+++ resistance.c
@@ -1,30 +1,79 @@
 #include <stdlib.h>
 #include "resistance.h"
 
+/*---------------------------------------------------------------------------*/
+static int invalidParamsSupplied(int count, char conn, float *array);
+static float getSerialResistorValue(int count, float *resistors);
+static float getParallelResistorValue(int count, float *resistors);
+
+/*---------------------------------------------------------------------------*/
 float calc_resistance(int count, char conn, float *array)
 {
   const int ERROR_CODE = -1;
   float resistance = 0;
   int i = 0;
 
-  if (count <= 0)
+  if (invalidParamsSupplied(count, conn, array))
     {
       return ERROR_CODE;
+    }
+
+  if ('S' == conn)
+    {
+      resistance = getSerialResistorValue(count, array);
+    }
+  else
+    {
+      resistance = getParallelResistorValue(count, array);
+    }
+
+  return resistance;
+}
+
+/*---------------------------------------------------------------------------*/
+int invalidParamsSupplied(int count, char conn, float *array)
+{
+  const int INVALID = 1;
+  const int VALID = 0;
+
+  if (count <= 0)
+    {
+      return INVALID;
     }
 
   if (conn != 'S' && conn != 'P')
     {
-      return ERROR_CODE;
+      return INVALID;
     }
 
   if (array == NULL)
     {
-      return ERROR_CODE;
+      return INVALID;
     }
 
+  return VALID;
+}
+
+/*---------------------------------------------------------------------------*/
+float getSerialResistorValue(int count, float *resistors)
+{
+  float resistance = 0;
+  int i = 0;
   for (i = 0; i < count; ++i)
     {
-      resistance += array[i];
+      resistance += resistors[i];
     }
   return resistance;
+}
+
+/*---------------------------------------------------------------------------*/
+float getParallelResistorValue(int count, float *resistors)
+{
+  float resistance = 0;
+  int i = 0;
+  for (i = 0; i < count; ++i)
+    {
+      resistance += 1.0/resistors[i];
+    }
+  return 1.0/resistance;
 }
--- test_resistance.c
+++ test_resistance.c
@@ -8,6 +8,8 @@ static void testErrorArrayNullPtr();
 static void testSingleSerial100OhmResistor();
 static void testSingleParallel100OhmResistor();
 static void testTwoSerial100OhmResistors();
+static void testTwoParallel100OhmResistors();
+static void testParallelResistorsThatGivesDecimalValue();
 
 /*---------------------------------------------------------------------------*/
 int main()
@@ -18,6 +20,8 @@ int main()
   testSingleSerial100OhmResistor();
   testSingleParallel100OhmResistor();
   testTwoSerial100OhmResistors();
+  testTwoParallel100OhmResistors();
+  testParallelResistorsThatGivesDecimalValue();
 
   exit(EXIT_SUCCESS);
 }
@@ -66,4 +70,22 @@ void testTwoSerial100OhmResistors()
   resistors[0] = 100;
   resistors[1] = 100;
   assert(200 == calc_resistance(2, 'S', resistors));
+}
+
+/*---------------------------------------------------------------------------*/
+void testTwoParallel100OhmResistors()
+{
+  float resistors[2];
+  resistors[0] = 100;
+  resistors[1] = 100;
+  assert(50 == calc_resistance(2, 'P', resistors));
+}
+
+/*---------------------------------------------------------------------------*/
+void testParallelResistorsThatGivesDecimalValue()
+{
+  float resistors[2];
+  resistors[0] = 25;
+  resistors[1] = 25;
+  assert(12.5 == calc_resistance(2, 'P', resistors));
 }
